d4faa5e92e5542aa380496fbbe4e90f93780ecb8
Simon Kerr <simon.p.kerr@gmail.com>
2020-12-09 15:23:19 +0000

    add framer motion tooltip

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { TooltipProvider, TooltipMarker, Tooltip } from './Tooltip';
+
+const App = () => (
+  <TooltipProvider>
+    <h3>Tooltip</h3>
+    <p>
+      Tool tips can be used for information <TooltipMarker id='tt1' />
+    </p>
+    <Tooltip target='tt1' role='tooltip'>
+      The tooltip contains a bit more information to help you understand what
+      something means
+    </Tooltip>
+    <p>
+      <TooltipMarker id='tt2' /> Here's another tooltip
+    </p>
+    <Tooltip target='tt2' role='tooltip'>
+      The tooltip is for the second marker, contains a load of other stuff, but
+      not too much, else that would be not very good, but just enough to be
+      satisfying.
+    </Tooltip>
+    <p style={{ textAlign: 'right' }}>
+      Here's another tooltip to the right <TooltipMarker id='tt3' />
+    </p>
+    <Tooltip target='tt3' role='tooltip'>
+      The tooltip is for the third marker, contains a load of other stuff, but
+      not too much, else that would be not very good, but just enough to be
+      satisfying.
+    </Tooltip>
+  </TooltipProvider>
+);
+
+export default App;
